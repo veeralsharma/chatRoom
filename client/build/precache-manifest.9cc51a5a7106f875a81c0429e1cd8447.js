df4969affa5a5126de2f395ab103982c5c0b3f7a
Veeral Sharma <43784167+veeralsharma@users.noreply.github.com>
2020-04-02 16:33:25 +0530

    Add files via upload

---
+++ client/build/precache-manifest.9cc51a5a7106f875a81c0429e1cd8447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053b9f92afc28d801c4ed4c96cf09d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00356e96202cfa80121f",
+    "url": "/static/css/main.e7639fc5.chunk.css"
+  },
+  {
+    "revision": "f5ce001ba59a3abda442",
+    "url": "/static/js/2.6e8d3d6e.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.6e8d3d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00356e96202cfa80121f",
+    "url": "/static/js/main.11f4c522.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
